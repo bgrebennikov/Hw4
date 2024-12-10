05349f3503d3ae7278c76adbb7f5d8d9a4d368fd
Boris <bgrebennikovv@gmail.com>
2024-12-10 14:36:05 +0400

    Task 1

--- src/main/java/com/github/bgrebennikovv/Main.java
+++ src/main/java/com/github/bgrebennikovv/Main.java
@@ -2,6 +2,16 @@ package com.github.bgrebennikovv;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Task 1
+        int humanAge = 24;
+        if (humanAge >= 18) {
+            System.out.printf("""
+                    Если возраст человека равен %s, то он совершеннолетний
+                    """, humanAge);
+        } else {
+            System.out.println("Он не достиг совершеннолетия, нужно немного подождать");
+        }
+
     }
 }
