398a53e4a2123e707b5067b91674ef2758729efb
Boris <bgrebennikovv@gmail.com>
2024-12-10 14:56:49 +0400

    Task 4

--- src/main/java/com/github/bgrebennikovv/Main.java
+++ src/main/java/com/github/bgrebennikovv/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         // Task 1
-        int humanAge = 24;
+        int humanAge = 25;
         if (humanAge >= 18) {
             System.out.printf("""
                     Если возраст человека равен %s, то он совершеннолетний
@@ -26,9 +26,34 @@ public class Main {
         int driverSpeed = 65;
 
         if (driverSpeed > 60) {
-            System.out.printf("\nЕсли скорость %s км/ч, придется заплатить штраф", driverSpeed);
+            System.out.printf("\nЕсли скорость %s км/ч, придется заплатить штраф\n", driverSpeed);
         } else {
             System.out.printf("\nЕсли скорость %s км/ч, можно ездить спокойно\n", driverSpeed);
+        }
+
+        // Task 4
+        if (humanAge >= 2 && humanAge <= 6) {
+            System.out.printf("""
+                    
+                    Если возраст человека равен: %s, то ему нужно ходить в детский сад.
+                    
+                    """, humanAge);
+        } else if (humanAge >= 7 && humanAge <= 17) {
+            System.out.printf("""
+                    
+                    Если возраст человека равен: %s, то ему нужно ходить в школу.
+                    """, humanAge);
+        } else if (humanAge >= 18 && humanAge <= 24) {
+            System.out.printf("""
+                    
+                    Если возраст человека равен: %s, то ему нужно ходить в университет.
+                    """, humanAge);
+        }
+        else if (humanAge > 24) {
+            System.out.printf("""
+                    
+                    Если возраст человека равен: %s, то ему пора ходить на работу.
+                    """, humanAge);
         }
 
     }
